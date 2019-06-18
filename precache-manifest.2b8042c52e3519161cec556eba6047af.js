9193fe5bb184cb6b1afba329480a87de06b4cf85
Jonas Kelstrup <jonaskelstrup@gmail.com>
2019-06-18 11:10:10 +0200

    Updates

---
+++ precache-manifest.2b8042c52e3519161cec556eba6047af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0763af5719fa83e5455a9107ae9a17",
+    "url": "/shades/index.html"
+  },
+  {
+    "revision": "cb262ff532d684f90875",
+    "url": "/shades/static/css/main.cebb48a9.chunk.css"
+  },
+  {
+    "revision": "038487e8174ece95b4c4",
+    "url": "/shades/static/js/2.264b3348.chunk.js"
+  },
+  {
+    "revision": "cb262ff532d684f90875",
+    "url": "/shades/static/js/main.b235a5cc.chunk.js"
+  },
+  {
+    "revision": "4178377392e26d3deef5",
+    "url": "/shades/static/js/runtime~main.16b7fa52.js"
+  }
+]);
